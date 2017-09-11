ce52fd13bfd4cdf03f4d3f4d6503cb92c2117847
Matthew Faw <matthewfaw@gmail.com>
2017-09-11 00:51:59 -0400

    Fixes some lingering problems from project restructure

--- db/query.py
+++ db/query.py
@@ -1,8 +1,4 @@
 from setup_mongo_connection import get_songs_collection
-from suggested_artists import get_related_artists
-from artists import get_artist_info
-from genres import get_genres
-from save import save
 
 mongo = get_songs_collection()
 
@@ -17,17 +13,3 @@ def get_all_artists():
             }}
         ])
     return cur
-
-# related_artist_dict = {}
-# genre_dict = {}
-# for doc in get_all_artists():
-    # artist = doc['_id']
-    # print artist
-    # info = get_artist_info(artist)
-    # if info != {}:
-        # artist_id = info['id']
-        # related_artist_dict[artist] = get_related_artists(artist_id=artist_id)
-        # genre_dict[artist] = get_genres(info=info)
-
-# save(related_artist_dict,'related_artists')
-# save(genre_dict,'genres')
---
+++ save_spotify_data.py
@@ -0,0 +1,19 @@
+from db import query as q
+from spotify import artists
+from spotify import suggested_artists as sug
+from spotify import genres
+from serialize import save
+
+related_artist_dict = {}
+genre_dict = {}
+for doc in q.get_all_artists():
+    artist = doc['_id']
+    print(artist)
+    info = artists.get_artist_info(artist)
+    if info != {}:
+        artist_id = info['id']
+        related_artist_dict[artist] = sug.get_related_artists(artist_id=artist_id)
+        genre_dict[artist] = genres.get_genres(info=info)
+
+save.save(related_artist_dict,'derp1')
+save.save(genre_dict,'derp2')
