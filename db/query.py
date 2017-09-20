4ad815924cb7c9fc7d355589c514248b46193129
Matthew Faw <matthewfaw@gmail.com>
2017-09-19 21:08:59 -0400

    Documents the return format of the get_all_artists() request

--- db/query.py
+++ db/query.py
@@ -2,6 +2,23 @@ from .setup_mongo_connection import get_songs_collection
 
 mongo = get_songs_collection()
 
+'''
+Get the cursor for all artists from the database
+
+Each document in the response is of format:
+    {
+    _id: artistName,
+    songs: [{
+            song: songName, 
+            album: albumName,
+            numListens: totalNumberOfListensToThisSong
+        },...],
+    numListens: totalNumberOfListensToTheArtist
+    }
+
+The documents are sorted in increasing lexicographic order
+i.e. artist "ZZZ" comes before artist "aaa"
+'''
 def get_all_artists():
     cur = mongo.aggregate([
         {"$group": {
